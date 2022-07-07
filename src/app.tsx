45a1f93c491f8cb785fcf39d1e88a7ba2889ee66
lengfangbing <lengfangbing@gmail.com>
2022-07-07 23:06:11 +0800

    feat: 优化渲染逻辑，默认初始就加载用户信息

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
+import Taro from '@tarojs/taro';
 import { Component } from "react";
 import "./app.less";
 import 'taro-ui/dist/style/index.scss'
+import UserInfoProvider from './common/components/user-info-provider';
+import React from 'react';
 class App extends Component {
   componentDidMount() {
-    wx.cloud.init();
+    Taro.cloud.init();
   }
 
   componentDidShow() {}
@@ -14,7 +17,11 @@ class App extends Component {
 
   // this.props.children 是将要会渲染的页面
   render() {
-    return this.props.children;
+    return (
+      <UserInfoProvider>
+        {this.props.children}
+      </UserInfoProvider>
+    );
   }
 }
 
